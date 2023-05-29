831fec3b8ca5bf007398598fc332dcadad52b68f
이승훈 <lsh901673@gmail.com>
2023-05-30 01:17:20 +0900

    refactor: active crawler

--- src/main/java/me/lsh/javacrawler/controller/CompetitionCrawlerController.java
+++ src/main/java/me/lsh/javacrawler/controller/CompetitionCrawlerController.java
@@ -17,10 +17,10 @@ public class CompetitionCrawlerController extends EventCrawlerController {
         super(crawlerService, crawlerReport);
     }
 
-    // 처음 실행 된 후 5초후 실행, 이후 이전에 실행된 task의 종료 시긴으로 부터 5분(300000) 후에 실행됨.
+    // 처음 실행 된 후 5초후 실행, 이후 이전에 실행된 task의 종료 시긴으로 부터 750분(45000000) 후에 실행됨.
     @Override
-    @Scheduled(fixedDelay = 300000, initialDelay = 5000)
+    @Scheduled(fixedDelay = 45000000, initialDelay = 5000)
     public void crawlAll() {
-//        super.crawlAll();
+        super.crawlAll();
     }
 }
--- src/main/java/me/lsh/javacrawler/controller/MoimCrawlerController.java
+++ src/main/java/me/lsh/javacrawler/controller/MoimCrawlerController.java
@@ -16,8 +16,8 @@ public class MoimCrawlerController extends EventCrawlerController {
     }
 
     // 처음 실행 된 후 30초(120000/2[=1분]/2) 후 실행, 이후 이전에 실행된 task의 종료 시긴으로 부터 1분(300000/5) 후에 실행됨.
-    @Scheduled(fixedDelay = 300000, initialDelay = 120000)
+    @Scheduled(fixedDelay = 45000000, initialDelay = 120000)
     public void crawlAll() {
-//        super.crawlAll();
+        super.crawlAll();
     }
 }
