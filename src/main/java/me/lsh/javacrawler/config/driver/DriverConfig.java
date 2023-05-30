81be286a382cfd2ebf501c90f1eade51a1eafa39
LEE-sh1673 <lsh901673@gmail.com>
2023-05-30 22:32:30 +0900

    chore: edit Web Driver Configuration
    - Change configuration which set driver path depends on operating system.
    - Change previous chrome driver(for MAC) and Add other os chrome drivers. (windows, linux)

--- src/main/java/me/lsh/javacrawler/config/driver/DriverConfig.java
+++ src/main/java/me/lsh/javacrawler/config/driver/DriverConfig.java
@@ -1,14 +1,9 @@
 package me.lsh.javacrawler.config.driver;
 
-import java.time.Duration;
-import java.time.LocalTime;
-import java.time.temporal.ChronoUnit;
-import java.time.temporal.TemporalUnit;
-import java.util.Collections;
-import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -18,7 +13,15 @@ public class DriverConfig {
 
     private static final String WEB_DRIVER_ID = "webdriver.chrome.driver";
 
-    private static final String WEB_DRIVER_PATH = "src/main/java/me/lsh/javacrawler/config/driver/chromedriver";
+    @Value("${webdriver.chrome.driver.windows}")
+    private String webDriverPathWindows;
+
+    @Value("${webdriver.chrome.driver.macos}")
+    private String webDriverPathMacOS;
+
+    @Value("${webdriver.chrome.driver.linux}")
+    private String webDriverPathLinux;
+
 
     @Bean
     @Scope("singleton")
@@ -31,9 +34,12 @@ public class DriverConfig {
         return driver;
     }
 
+
     @Bean
     public ChromeOptions chromeOptions() {
-        System.setProperty(WEB_DRIVER_ID, WEB_DRIVER_PATH);
+        String os = System.getProperty("os.name").toLowerCase();
+        System.setProperty(WEB_DRIVER_ID, determineWebDriverPath(os));
+
         ChromeOptions options = new ChromeOptions();
         options.addArguments("lang=ko");
         options.addArguments("--no-sandbox");
@@ -44,7 +50,20 @@ public class DriverConfig {
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--disable-popup-blocking");
         options.addArguments("--remote-allow-origins=*");
-        options.addArguments("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/605.1.15");
+        options.addArguments(
+            "user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/605.1.15");
         return options;
+    }
+
+    private String determineWebDriverPath(String os) {
+        if (os.contains("win")) {
+            return webDriverPathWindows;
+        } else if (os.contains("mac")) {
+            return webDriverPathMacOS;
+        } else if (os.contains("nix") || os.contains("nux") || os.contains("aix")) {
+            return webDriverPathLinux;
+        } else {
+            throw new IllegalStateException("Unsupported operating system: " + os);
+        }
     }
 }
